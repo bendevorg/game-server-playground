c6c724bd808a9082b287f30d4eae366d85148620
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-11-25 01:47:17 -0500

    Tweak movement to be closer to client's movement

--- server/controllers/entity/move.ts
+++ server/controllers/entity/move.ts
@@ -1,28 +1,45 @@
 import { Player } from '../../interfaces';
 import { game } from '../../constants';
+import path from 'path/posix';
 
 export default (player: Player) => {
   if (!player.path) {
     return;
   }
-  const distanceX = player.path.waypoints[player.path.waypoints.length - 1].x - player.position.x;
-  const distanceZ = player.path.waypoints[player.path.waypoints.length - 1].z - player.position.z;
-  const magnitude = Math.sqrt(distanceX * distanceX + distanceZ * distanceZ);
-  if (magnitude <= game.MIN_DISTANCE_FOR_NEXT_WAYPOINT) {
+  let distanceX =
+    player.path.waypoints[player.path.waypoints.length - 1].x -
+    player.position.x;
+  let distanceZ =
+    player.path.waypoints[player.path.waypoints.length - 1].z -
+    player.position.z;
+  let magnitude = Math.sqrt(distanceX * distanceX + distanceZ * distanceZ);
+
+  while (
+    magnitude <= game.MIN_DISTANCE_FOR_NEXT_WAYPOINT &&
+    player.path.waypoints.length > 1
+  ) {
+    player.path.waypoints.pop();
+    distanceX =
+      player.path.waypoints[player.path.waypoints.length - 1].x -
+      player.position.x;
+    distanceZ =
+      player.path.waypoints[player.path.waypoints.length - 1].z -
+      player.position.z;
+    magnitude = Math.sqrt(distanceX * distanceX + distanceZ * distanceZ);
+  }
+
+  if (magnitude <= game.MIN_DISTANCE_FOR_NEXT_WAYPOINT && player.path.waypoints.length === 1) {
     player.position = {
       ...player.position,
-      x: player.path.waypoints[player.path.waypoints.length - 1].x,
-      z: player.path.waypoints[player.path.waypoints.length - 1].z,
-    }
+      x: player.path.waypoints[0].x,
+      z: player.path.waypoints[0].z,
+    };
     player.path.waypoints.pop();
-    if (player.path.waypoints.length === 0) {
-      player.path = undefined;
-    }
     return;
   }
   const directionX = distanceX / magnitude;
   const directionZ = distanceZ / magnitude;
-  const timeSinceLastUpdate = ((new Date()).getTime() - player.lastUpdate) / 1000;
+  const timeSinceLastUpdate = (new Date().getTime() - player.lastUpdate) / 1000;
   const distanceToMoveInX = directionX * player.speed * timeSinceLastUpdate;
   const distanceToMoveInZ = directionZ * player.speed * timeSinceLastUpdate;
   player.position = {
